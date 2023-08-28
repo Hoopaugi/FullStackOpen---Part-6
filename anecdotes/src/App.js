36de6271fc00264e5605ef21c7b4b11032ba84fb
Hoopaugi <hoopaugi@gmail.com>
2023-08-28 10:43:45 +0300

    Progress

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -1,8 +1,21 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import Anecdotes from './components/Anecdotes'
 import NewAnecdote from './components/NewAnecdote'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <div>
       <h1>Anecdotes</h1>
